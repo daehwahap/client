2d0230b625ac89be8b8704eb4dd0ffe2e48f94d4
bongsu-rapportlabs <bongsu@rapportlabs.kr>
2024-04-13 11:23:26 +0900

    fix: debug 에러 제거

--- server/daehwahap-server/src/trpc/trpc.module.ts
+++ server/daehwahap-server/src/trpc/trpc.module.ts
@@ -2,10 +2,10 @@ import { Module, forwardRef } from '@nestjs/common'
 import { TrpcService } from './trpc.service'
 import { TrpcRouter } from './trpc.router'
 import { UserModule } from 'src/trpc/user/user.module'
+import { UserController } from './user/user.controller'
 
 @Module({
   imports: [forwardRef(() => UserModule)],
   controllers: [],
-  providers: [TrpcService, TrpcRouter],
-})
+  providers: [TrpcService, TrpcRouter, UserController], })
 export class TrpcModule {}
